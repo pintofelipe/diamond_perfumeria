d0f8d3d0d9b9a5eff503894ccf6dc3c1cf63852c
pintofelipe <afpintou@ufpso.edu.co>
2025-02-23 20:20:50 -0500

    feat: add project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className="text-3xl font-bold underline text-center">
+    diamond_perfumeria 77
+   </h1>
+    </>
+  )
+}
+
+export default App
