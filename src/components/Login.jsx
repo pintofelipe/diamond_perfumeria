6ae8277d585cc26d0537488752119e05441312f8
Juanjo <jjgarciat@ufpso.edu.co>
2025-04-06 21:31:36 -0500

    Creacion del Login

---
+++ src/components/Login.jsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onLogin = () => {
+        console.log("Login con:", { email, password,});
+        // Aquí conectas con tu backend o autenticador
+      };
+    
+    const loginWithGoogle = () => {
+        console.log("Login con Google");
+        // Aquí conectas con Google Auth
+    };
+    
+    const navegarMain = () => {
+        console.log("Navegar al Main");
+        // Aquí puedes usar router.push('/main') si usas useRouter de Next.js
+    };
+    
+    const navegarRegister = () => {
+        console.log("Navegar al Registro");
+    };
+
+  return (
+    <div className="flex justify-center items-center w-full h-screen overflow-hidden">
+    <div className="bg-[#1e1e1e] relative w-full h-full flex justify-center items-center">
+      
+      {/* Logo Header */}
+      <div
+        onClick={navegarMain}
+        className="absolute top-0 w-full p-4 flex items-center space-x-2 cursor-pointer"
+      >
+        <h1 className="text-4xl italic font-semibold text-[#facc15]">Diamond</h1>
+        <img
+          src="/Diamond.png"
+          alt="logo"
+          width={52}
+          height={52}
+          className="text-[#facc15]"
+        />
+      </div>
+
+      {/* SVG Wave */}
+      <svg
+        className="absolute bottom-0 left-0 w-full text-[#facc15]"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          fill="currentColor"
+          fillOpacity="1"
+          d="M0,256L60,234.7C120,213,240,171,360,170.7C480,171,600,213,720,234.7C840,256,960,256,1080,245.3C1200,235,1320,213,1380,202.7L1440,192V320H1380C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320H0Z"
+        ></path>
+      </svg>
+
+      {/* Login Card */}
+      <div className="relative z-10 mt-16 p-8 bg-[#2a2a2a] rounded-xl shadow-3xl border border-[#facc15] shadow-[0_0_10px_2px_#facc15]">
+        <img
+          src="/Diamond.png"
+          alt="logo"
+          width={90}
+          height={90}
+          className="text-[#facc15] m-auto"
+        />
+        <h2 className="text-2xl font-semibold italic text-center text-[#facc15] mb-6">
+          Inicio de Sesión
+        </h2>
+
+        {/* Email */}
+        <div className="mb-4">
+          <label className="block text-white font-semibold italic mb-1">
+            Usuario
+          </label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full px-4 py-2 border rounded-lg shadow-2xl focus:ring-2 focus:ring-[#facc15] focus:outline-none transition text-amber-50 font-semibold italic"
+            placeholder="Ingrese su usuario"
+          />
+        </div>
+
+        {/* Password */}
+        <div className="mb-4">
+          <label className="block text-white font-semibold italic mb-1">
+            Contraseña
+          </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full px-4 py-2 border rounded-lg shadow-2xl focus:ring-2 focus:ring-[#facc15] focus:outline-none transition text-amber-50 font-semibold italic"
+            placeholder="Ingrese su contraseña"
+          />
+        </div>
+
+        {/* Login Button */}
+        <button
+          onClick={onLogin}
+          className="w-full bg-[#facc15] text-black font-semibold py-2 rounded-lg shadow-md hover:bg-[#fde047] transition cursor-pointer"
+        >
+          Iniciar Sesión
+        </button>
+
+        {/* Google Login */}
+        <button
+          onClick={loginWithGoogle}
+          className="w-full bg-[#facc15] text-black font-semibold py-2 rounded-lg shadow-md hover:bg-[#fde047] transition cursor-pointer mt-2"
+        >
+          Iniciar Sesión con Google
+        </button>
+
+        {/* Enlaces extra */}
+        <div className="mt-4 text-sm text-center text-gray-600 flex gap-2 justify-center flex-wrap">
+          <p
+            onClick={navegarRegister}
+            className="text-[#facc15] font-semibold italic hover:underline cursor-pointer"
+          >
+            ¿No tienes una cuenta?
+          </p>
+          <span>|</span>
+          <p className="text-[#facc15] font-semibold italic hover:underline cursor-pointer">
+            ¿Se te olvidó tu contraseña?
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Login
