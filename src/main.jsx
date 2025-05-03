81fd0532d798fd2b04dd4e0794bff7fdd792f178
Juanjo <jjgarciat@ufpso.edu.co>
2025-05-03 14:38:11 -0500

    Correciones 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Login from './components/Login.jsx'
-import Register from './components/Register.jsx'
-import Index from './components/Index.jsx'
-import Admin from './components/admin.jsx'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./contexts/CartProvider.jsx";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import Cart from "./components/Cart.jsx";
+import Index from "./components/index.jsx";
+import Checkout from "./components/Checkout.jsx";
+import Confirmation from "./components/Confirmation.jsx";
+import Admin from "./components/admin.jsx";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Index" element={<Index />} />
-        <Route path="/Admin" element={<Admin />} />
-      </Routes>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/Index" element={<Index />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="/Checkout" element={<Checkout />} />
+          <Route path="/Confirmation" element={<Confirmation />} />
+          <Route path="/Admin" element={<Admin />} />
+        </Routes>
+      </CartProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
